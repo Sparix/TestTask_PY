5d772bd171333a25bd1df68ba577e31df00ed51b
Olexandr Hontarenko <sanya221138@gmail.com>
2024-03-30 23:12:32 +0200

    db query optimisation and refactoring

--- show_data.py
+++ show_data.py
@@ -1,4 +1,6 @@
-from sqlalchemy import distinct, func
+import typing
+
+from sqlalchemy import func
 from sqlalchemy.orm import Session, Query
 
 from database.db import engine
@@ -8,34 +10,45 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-def get_info_region(query: Query, region: str) -> dict:
-    info_region = {
+def get_region_data(session: Session, region_data: typing.Any) -> dict:
+    print(region_data)
+    smallest_country = (
+        session.query(models.Country.name, models.Country.population)
+        .filter(models.Country.region == region_data.region,
+                models.Country.population == region_data.smallest_population)
+        .first()
+    )
+    biggest_country = (
+        session.query(models.Country.name, models.Country.population)
+        .filter(models.Country.region == region_data.region,
+                models.Country.population == region_data.biggest_population)
+        .first()
+    )
+    return {
+        "name_region": region_data.region,
+        "total_population": region_data.total_population,
+        "smallest_name_country": smallest_country.name,
+        "smallest_population_country": smallest_country.population,
+        "biggest_name_country": biggest_country.name,
+        "biggest_population_country": biggest_country.population
     }
-    total_population = query.with_entities(
-        func.sum(models.Country.population)
-    ).scalar()
-    biggest_country = query.order_by(models.Country.population.desc()).first()
-    smallest_country = query.order_by(models.Country.population).first()
-    info_region["name_region"] = region
-    info_region["total_population"] = total_population
-    info_region["smallest_name_country"] = smallest_country.name
-    info_region["smallest_population_country"] = smallest_country.population
-    info_region["biggest_name_country"] = biggest_country.name
-    info_region["biggest_population_country"] = biggest_country.population
-    return info_region
 
 
 @app.route("/")
 def show_data():
     with Session(engine) as session:
-        countries = session.query(models.Country)
-        regions = countries.distinct(models.Country.region).all()
-        context_region_info = []
-        for region in regions:
-            region_query = countries.filter(models.Country.region == region.region)
-            info_region = get_info_region(region_query, region.region)
-            context_region_info.append(info_region)
-    print("working it's strange")
+        region_info = (
+            session.query(
+                models.Country.region,
+                func.sum(models.Country.population).label("total_population"),
+                func.min(models.Country.population).label("smallest_population"),
+                func.max(models.Country.population).label("biggest_population")
+            )
+            .group_by(models.Country.region)
+            .all()
+        )
+        context_region_info = [get_region_data(session, region_data) for region_data in region_info]
+
     return render_template("show_data.html", info=context_region_info)
 
 
